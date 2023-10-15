d7e2330bf3d3c6845c314dc43977eb8ebdc2a9ce
Kobe <kobechua14@gmail.com>
2023-10-15 18:03:26 -0400

    Initial Commit

---
+++ To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Kobe on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
